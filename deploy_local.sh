9bf003b57fa70350f80c3edd38cf0f334c5d2e46
Charlie Powell <cdp1337@veraciousnetwork.com>
2024-01-07 20:08:50 -0500

    Add support for debugging on a local server

--- deploy_local.sh
+++ deploy_local.sh
@@ -8,7 +8,8 @@ HERE="$(realpath "$(dirname "$0")")"
 source "$HERE/settings.sh"
 
 # Set target directory for local testing
-DEST="$HOME/Zomboid/mods/$MOD_NAME"
+DEST="$HOME/Zomboid/mods/DEV_$MOD_NAME"
+SERVER="$HOME/Zomboid/Server"
 
 if [ ! -d "$DEST" ]; then
 	mkdir -p "$DEST"
@@ -32,4 +33,17 @@ if [ -n "$PACKS" ]; then
 	for PACK in $PACKS; do
 		cp "$HERE/supplemental/Packs/$PACK" "$DEST/media/texturepacks/$PACK"
 	done
+fi
+
+# Swap target name to differentiate between production version and dev version.
+# This causes issues otherwise if the Workshop version is subscribed to;
+# that version will take precedence over the local version.
+sed -i "s/id=.*/id=DEV_$MOD_NAME/" "$DEST/mod.info"
+
+
+# Check if there is a local server available, (useful for testing server-side code in a local environment)
+if [ -e "$SERVER/servertest.ini" ]; then
+	if ! egrep -q "^Mods=.*DEV_$MOD_NAME" "$SERVER/servertest.ini"; then
+		sed -i "s/Mods=.*/Mods=DEV_$MOD_NAME/" "$SERVER/servertest.ini"
+	fi
 fi
